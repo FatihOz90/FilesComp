76b771a5980033d75fdc16fe973df2093e244eaa
FatihOz90 <sanalturkey@gmail.com>
2020-07-06 10:27:01 +0300

    Add files via upload

---
+++ Untitled1.ps1
@@ -0,0 +1,10 @@
+﻿$TXT_A = Get-Content "a.txt"
+$TXT_B = Get-Content "b.txt"
+$TXT_C = ".\c.txt"
+
+$Lookup = @{"==" = "Esit"}
+
+Compare-Object -ReferenceObject $TXT_A -DifferenceObject $TXT_B -IncludeEqual                                  |
+   Select @{Name="String";Expression={$_.InputObject}},@{Name="Durum";Expression={$Lookup[$_.SideIndicator]}}  |
+   Where-Object {$_.Durum -eq "Esit"}                                                                          |
+   Format-Table String | Out-File $TXT_C
